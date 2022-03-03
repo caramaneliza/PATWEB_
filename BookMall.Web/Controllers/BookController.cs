3e0a0f1673093ab1296152e7f00ac372e1d84cb2
catalinplesu <catalinhimself@gmail.com>
2022-03-03 07:55:40 -0800

    visual template replicated

---
+++ BookMall.Web/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookMall.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        [Route("book/{id?}")]
+        public ActionResult Index(string id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+    }
+}
---
+++ BookMall.Web/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookMall.Web.Models
+{
+    public class Book
+    {
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public float Price { get; set; }
+        public int Pages { get; set; }
+        
+    }
+}
