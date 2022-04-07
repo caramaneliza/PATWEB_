51ef9bb07a48f6297921d93367ade455aa97f5dc
catalinplesu <catalinhimself@gmail.com>
2022-04-07 10:33:07 -0700

    working, problems installing the mssql malware

---
+++ BookMall.BuisnessLogic/DBModel/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using BookMall.Domain.Entities.User;
+
+namespace BookMall.BuisnessLogic.DBModel
+{
+    class UserContext :  DbContext
+    {
+        public UserContext() :
+                    base("name=BookMall") // connectionstring name define in your web.config
+        {
+        }
+
+        public virtual DbSet<UDbTable> Users { get; set; }
+    }
+}
